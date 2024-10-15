cf8e97f5ccfc355aaf5ac4bb7b50f6ec6d89a8ef
Anna Cardinal <anna.cardinal1717@gmail.com>
2024-10-15 11:29:27 -0700

    get/set code added

--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -1,5 +1,84 @@
 package com.pluralsight;
 
-public class Ledger {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class Ledger {
+    LocalDateTime today = LocalDateTime.now();
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss");
+    String formatedDate = today.format(fmt);
+    String description;
+    String vendor;
+    double amount;
+    int securityPin;
+    long cardNumber;
+    String expoDate;
+
+public Ledger(String formatedDate, String description, String vendor, double amount) {
+    this.formatedDate = formatedDate;
+    this.description = description;
+    this.vendor = vendor;
+    this.amount = amount;
 }
+public String getformatedDate(){
+    return formatedDate;
+    }
+public void setFormatedDate(String formatedDate){
+    this.formatedDate = formatedDate;
+    }
+public String getDescription(){
+    return description;
+}
+public void setDescription(String description){
+    this.description = description;
+}
+public String getVendor(){
+    return vendor;
+}
+public void setVendor(String vendor){
+    this.vendor = vendor;
+}
+public double getAmount() {
+        return amount;
+}
+public void setAmount(double amount) {
+        this.amount = amount;
+}
+private paymentMethed(long cardNumber, String expoDate, int securityPin){
+    this.cardNumber = cardNumber;
+    this.expoDate = expoDate;
+    this.securityPin = securityPin;
+}
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(long cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+    public String getExpoDate(){
+        return expoDate;
+    }
+
+    public void setExpoDate(String expoDate) {
+        this.expoDate = expoDate;
+    }
+
+    public int getSecurityPin() {
+        return securityPin;
+    }
+
+    public void setSecurityPin(int securityPin) {
+        this.securityPin = securityPin;
+    }
+}
+//@Override
+//    public String toString() {
+//        return "Ledger{" +
+//                "formatedDate=" + formatedDate +
+//                ", description='" + description + '\'' +
+//                ", vendor='" + vendor + '\'' +
+//                ", amount=" + amount +
+//                '}';
+//}
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int command;
@@ -56,9 +57,17 @@ public class Main {
             LocalDateTime today = LocalDateTime.now();
             DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss");
             String formatedDate = today.format(fmt);
+            System.out.println("16 digit card number: ");
+            long cardNumber = scanner.nextLong();
+            System.out.println("Experation date: ");
+            String expoDate = scanner.nextLine();
+            scanner.nextLine();
+            System.out.println("3 digit security pin: ");
+            int securityPin = scanner.nextInt();
             System.out.println("Description: ");
             String description = scanner.nextLine();
-            System.out.println("Vendor: ");
+            scanner.nextLine();
+            System.out.println("Payment by: ");
             String vendor = scanner.nextLine();
             System.out.println("Price: ");
             double price = scanner.nextDouble();
